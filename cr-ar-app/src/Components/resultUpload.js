f4f1e2248bf2da79d95fffa470a09b2b08c38515
Aarthy <2019e001@eng.jfn.ac.lk>
2023-07-20 15:43:59 +0530

    result

--- cr-ar-app/src/Components/resultUpload.js
+++ cr-ar-app/src/Components/resultUpload.js
@@ -4,43 +4,43 @@ import MainHead from "./MainHead";
 import "../Styles/ResultStyle.css";
 
 
-function ResultUpload(){
-    const [file, setFile]=useState()
+function ResultUpload() {
+    
+    const [file, setFile] = useState()
 
-    function handleFile(event){
+    function handleFile(event) {
         setFile(event.target.files[0])
         console.log(event.target.files[0])
     }
-    function handleUpload(){
+    function handleUpload() {
         const formData = new FormData()
-        formData.append('file',file)
+        formData.append('file', file)
         fetch(
             'url',
             {
                 method: "POST",
                 body: formData
             }
-        ).then((response)=>response.json())
-        .then(
-            (result)=>{
-                console.log('success', result)
-            }
-        )
-        .catch(error=>{
-            console.error("Error:", error)
-        })
+        ).then((response) => response.json())
+            .then(
+                (result) => {
+                    console.log('success', result)
+                }
+            )
+            .catch(error => {
+                console.error("Error:", error)
+            })
     }
 
-    return(
+    return (
+        <div className="center-container">
 
-        <div>
-            <MainHead title="Upload a file"/>
-
-            <h2>Upload a file</h2>
+            <h2>Upload the result</h2>
             <form onSubmit={handleUpload}>
                 <input type="file" name="file" onChange={handleFile}/>
                 <button>Upload</button>
             </form>
+            <MainHead title="Courses" searchTitle="Search Courses..." isBtn="0"/>
         </div>
     )
 }
