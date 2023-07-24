270ef198768b49718f0ceb15667119a941563818
Aarthy <2019e001@eng.jfn.ac.lk>
2023-07-24 21:44:19 +0530

    download button

--- cr-ar-app/src/Components/DownloadButton.js
+++ cr-ar-app/src/Components/DownloadButton.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import '../Styles/StudentStyle.css';
 import '../Styles/DownloadButton.css';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 function DownloadCourses({ onClick }) {
+
+  //const pdfRef = useRef();
+
  
   return (
     <button className="downstyle" onClick={onClick}>
