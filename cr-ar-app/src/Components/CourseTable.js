71c45ecd2f2d9fdb00a4ce76c8ee5e9a3337fe8e
Aarthy <2019e001@eng.jfn.ac.lk>
2023-07-26 15:24:30 +0530

    downloadButton

--- cr-ar-app/src/Components/CourseTable.js
+++ cr-ar-app/src/Components/CourseTable.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect, useRef } from 'react';
 import "../Styles/StudentStyle.css";
 import SearchBar from './SearchBar';
 //import React, {useRef} from "react";
+import html2pdf from 'html2pdf.js';
+import { useReactToPrint } from 'react-to-print';
+
 
 import DropDownYear from "./DropDownYear";
 
@@ -11,8 +14,7 @@ import Table from "./Table";
 import '../Styles/ExtraFeatureButton.css';
 import MainHead from "./MainHead";
 import '../Styles/DownloadButton.css';
-
-
+import ReactDOM from 'react-dom';
 
 import DropDownDepartment from "./DropDownDepartment";
 import DropDownSemester from "./DropDownSemester";
@@ -20,29 +22,60 @@ import DownloadButton from "./DownloadButton";
 import ExtraFeatureButton from "./ExtraFeatureButton";
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
-
-
   
   function CourseTable() {
 
+    const conponentPDF=useRef();
+    const[userData, setUserdata]=useState([]);
+
+    const generatePDF = useReactToPrint({
+      content: () => conponentPDF.current,
+      documentTitle: "Userdata",
+      onAfterPrint: () => alert("Data saved in PDF"),
+      pageStyle: `
+        @page {
+          size: 1920px 1080px; /* Set the desired page size (width x height) */
+          margin: 0; /* Set margin to 0 to remove default page margins */
+          transform: scale(0.5); /* Adjust the zoom level by changing the scale value */
+        }
+        @media print {
+          body {
+            margin: 0; /* Set margin to 0 to remove default body margins */
+          }
+        }
+      `,
+    });
+  
     const pdfRef = useRef();
 
-  /*  const downloadPDF=()=>{
-      const input = pdfRef.current;
-      html2canvas(input).then((canvas)=>{
-        const imgData=canvas.toDataURL('image/png');
-        const pdf=new jsPDF('p','mm', 'a4', true);
-        const pdfwidth=pdf.internal.pageSize.getWidth();
-        const pdfHeight=pdf.internal.pageSize.getHeight();
-        const imgWidth=canvas.width;
-        const imgHeight=canvas.height;
-        const ratio=Math.min(pdfwidth/imgWidth, pdfHeight/imgHeight);
-        const imgX=(pdfwidth-imgWidth*ratio)/2;
-        const imgY=30;
-        pdf.addImage(imgData, 'PNG', imgX, imgY, imgWidth*ratio, imgHeight*ratio);
-        pdf.save('invoice.pdf');
-      });
-    };*/
+    // Function to handle the printing functionality
+    const handlePrint = () => {
+    const tableElement = pdfRef.current.querySelector('.table'); // Assuming the table has the 'table' class
+    if (tableElement) {
+      const content = tableElement.outerHTML;
+      const printWindow = window.open('', '', 'height=500,width=800');
+      printWindow.document.write('<html><head><title>Print</title></head><body>');
+      printWindow.document.write(content);
+      printWindow.document.write('</body></html>');
+      printWindow.document.close();
+      printWindow.print();
+    }
+  };
+
+  const handlePrint1 = () => {
+    const tableElement = pdfRef.current.querySelector('.table');
+    const content = tableElement.outerHTML;
+    const opt = {
+      margin: 10,
+      filename: 'course_details.pdf',
+      image: { type: 'jpeg', quality: 1 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'pt', format: 'a4', orientation: 'portrait' },
+    };
+
+    html2pdf().set(opt).from(content).save();
+    
+  };
     
     const handleSearch = (searchTerm) => {
       // Perform search logic here using the searchTerm
@@ -70,15 +103,6 @@ import jsPDF from 'jspdf';
     const handleExtra = (viewing) =>{
       console.log('view:', viewing);
     }
-    /*const list = [
-    { Course: "Software Construction",Code: "EC1010",Credit: "3",Core: "Core",Coordinator: "jananie",Prerequiste: "Operating System", RegistrationDate: "10.01.2023-30.01.2023", Status: "Close"},
-    { Course: "Software Construction",Code: "EC1010",Credit: "3",Core: "Core",Coordinator: "jananie",Prerequiste: "Operating System", RegistrationDate: "10.01.2023-30.01.2023", Status: "Close"},
-    { Course: "Software Construction",Code: "EC1010",Credit: "3",Core: "Core",Coordinator: "jananie",Prerequiste: "Operating System", RegistrationDate: "10.01.2023-30.01.2023", Status: "Close"},
-    { Course: "Software Construction",Code: "EC1010",Credit: "3",Core: "Core",Coordinator: "jananie",Prerequiste: "Operating System", RegistrationDate: "10.01.2023-30.01.2023", Status: "Close"},
-    { Course: "Software Construction",Code: "EC1010",Credit: "3",Core: "Core",Coordinator: "jananie",Prerequiste: "Operating System", RegistrationDate: "10.01.2023-30.01.2023", Status: "Close"},
-    { Course: "Software Construction",Code: "EC1010",Credit: "3",Core: "Core",Coordinator: "jananie",Prerequiste: "Operating System", RegistrationDate: "10.01.2023-30.01.2023", Status: "Close"},
-    { Course: "Software Construction",Code: "EC1010",Credit: "3",Core: "Core",Coordinator: "jananie",Prerequiste: "Operating System", RegistrationDate: "10.01.2023-30.01.2023", Status: "Close"}
-  ];*/
   
   const colNames = ['Course Code','Course Name','Credit','Core/Technical','Coordinator','Prerequiste','Offered Dept Name','Sem start Date', 'Sem End Date'];
     
@@ -97,27 +121,26 @@ import jsPDF from 'jspdf';
 
     return (
       
-      <div ref={pdfRef}>
+      <div ref={pdfRef}> 
+        <div ref={conponentPDF} style={{width:'100%'}}>      
         
-        <div>
         <Table list={Data} colNames={colNames}/>
+        </div>
+        <div>
         
         <MainHead title="Courses" searchTitle="Search Courses..." isBtn="0"/>
         </div>
        
         <div>
-          <DownloadButton onClick={handledownload} />
+          <DownloadButton onClick={generatePDF} />
         </div>
         Course details
+        
       </div>
-           
     );
   };
-  
+
   export default CourseTable;
-/*
- <div className='downstyle'>
-          <button className="btn btn-primary" onClick={downloadPDF}>Download PDF</button>
-        </div>*/
+
 
        
