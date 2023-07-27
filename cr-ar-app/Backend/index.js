4b25eb0bea7501898db977ee94279a7c3ae54b3d
Senthooran <2019e132@eng.jfn.ac.lk>
2023-07-27 07:24:29 +0530

    studentRegisterFormUpdated

--- cr-ar-app/Backend/index.js
+++ cr-ar-app/Backend/index.js
@@ -475,30 +475,69 @@ app.post("/insertNewSemester", (req, res) => {
 
 
     // student register
+    let updatedS = "";
 
-  let updatedS = "";
-
-  app.post("http://localhost:3300/studentBio", (req, res) => {
-  const formData = req.body;
-  //const {email,indexNo,nic,fname,lname,mathematics,physics,chemistry,english,phno,dob,zscore,rank,address,medium} = formData;
-  const sql =
-  "INSERT INTO student_registration_sample (`Email`, `IndexNo`, `NIC`, `FName`, `LName`, `Mathematics`, `Physics`, `Chemistry`, `English`, `PhoneNum`, `BOB`, `Z-Score`, `Rank`, `Address`, `Medium`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-  db.query(sql, [formData], (err, resultS) => {
-    if (err) {
-      console.log("Error:", err);
-      return res.status(500).json({ error: "Database error" });
-    }
-    console.log("Student registration table updated successfully");
-    console.log("Result:", resultS);
-
-    // Send the result as a response to the client
-    updatedS = resultS;
-    return res.status(200).json(resultS);
+    app.post("/insertNewStudent", (req, res) => {
+      const formData = req.body;
+      const {
+        email,
+        indexNo,
+        nic,
+        fname,
+        lname,
+        mathematics,
+        physics,
+        chemistry,
+        english,
+        phno,
+        dob,
+        zscore,
+        rank,
+        address,
+        medium
+      } = formData;
+    
+      const sql =
+        "INSERT INTO student_registration_sample (`Email`, `IndexNo`, `NIC`, `FName`, `LName`, `Mathematics`, `Physics`, `Chemistry`, `English`, `PhoneNum`, `BOB`, `Z-Score`, `Rank`, `Address`, `Medium`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    
+      db.query(
+        sql,
+        [
+          email,
+          indexNo,
+          nic,
+          fname,
+          lname,
+          mathematics,
+          physics,
+          chemistry,
+          english,
+          phno,
+          dob,
+          zscore,
+          rank,
+          address,
+          medium,
+        ],
+        (err, resultS) => {
+          if (err) {
+            console.log("Error:", err);
+            return res.status(500).json({ error: "Database error" });
+          }
+          console.log("Student registration table updated successfully");
+          console.log("Result:", resultS);
+    
+          // Send the result as a response to the client
+          updatedS = resultS;
+          return res.status(200).json(resultS);
+        }
+      );
     });
-  });
-  app.get("/bio",(req,res)=>{
-    return res.json(updatedS);
-  })
+    
+    app.get("/bio", (req, res) => {
+      return res.json(updatedS);
+    });
+    
 
 
 
