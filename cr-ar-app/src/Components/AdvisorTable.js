3916de552da89c6f4cc28d2c3b42d3b0558ba7b4
RizaSubair <2019e114@eng.jfn.ac.lk>
2023-07-27 07:14:47 +0530

    popup added

--- cr-ar-app/src/Components/AdvisorTable.js
+++ cr-ar-app/src/Components/AdvisorTable.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState }  from "react";
 import "../Styles/AdvisorTableStyles.css";
 import "../Styles/Tickbox.css";
+import "../Styles/PopupStyles.css";
+import axios from "axios";
 
 function AdvisorTable({list, colNames, width = 'auto', height = 'auto'}) {
+    const [showPopup, setShowPopup] = useState(false);
+    const [selectedRow, setSelectedRow] = useState(null);
+    const [advisorHistory, setAdvisorHistory] = useState(null);
+
+    const handleRowClick = (rowData, index) => {
+        setSelectedRow(rowData);
+        setShowPopup(true);
+
+        const regNo = rowData.RegNo;
+        
+        axios.post("http://localhost:3300/advisorHistory", {
+        RegNo: regNo
+        })
+        .then((response) => {
+        // Handle the response here, set it to state or perform any other actions
+        console.log("Additional information:", response.data);
+        setAdvisorHistory(response.data);
+        })
+        .catch((error) => {
+        console.error("Error getting additional information:", error);
+        });
+    };
+
+    const closePopup = () => {
+        setShowPopup(false);
+        setSelectedRow(null);
+        setAdvisorHistory(null);
+    };
+
     return (
-        <>
         <div className="advtable-wrapper">
             {list.length > 0 && (
                 <table className="advtable">
@@ -27,8 +57,7 @@ function AdvisorTable({list, colNames, width = 'auto', height = 'auto'}) {
                                 </label>
                                 </td>
                                 {Object.values(obj).map((value,index2) => (
-                                    
-                                    <td key={index2}>
+                                    <td key={index2} onClick={() => handleRowClick(value,index2)} >
                                         {value}
                                     </td>
                                 ))}
@@ -37,8 +66,33 @@ function AdvisorTable({list, colNames, width = 'auto', height = 'auto'}) {
                     </tbody>
                 </table>
             )}
+            {showPopup && selectedRow &&  (
+                    <div className="popup">
+                        <div className="popup-content">
+                        <h2>Advisor History:</h2>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Advisor</th>
+                                    <th>Start Date</th>
+                                    <th>End Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {advisorHistory && advisorHistory.map((advisor, index) => (
+                                    <tr key={index}>
+                                        <td>{advisor.StaffName}</td>
+                                        <td>{advisor.StartDate}</td>
+                                        <td>{advisor.EndDate}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                        <button className="close" onClick={closePopup}>X</button>
+                        </div>
+                    </div>
+            )}
         </div>
-        </>
     );
 }
 
